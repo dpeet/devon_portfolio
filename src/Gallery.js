02fb18338c8c2c0acd5ac4de3a1ee64c07f53916
Devon Peet <bearbiever@gmail.com>
2017-08-29 16:05:40 -0400

    code cleanup

--- src/Gallery.js
+++ src/Gallery.js
@@ -11,7 +11,7 @@ const Gallery = () => (
   <li className="cards__item">
     <div className="card">
       <Link to='/dndtower'>
-        <div className="card__image card__image--dnd"></div>
+        <div className="card__image card__image--dnd"> </div>
         <div className="card__content">
           <div className="card__title">Dungeons and Dragons Dice Tower</div>
           <p className="card__text">Spring 2017</p>
@@ -22,7 +22,7 @@ const Gallery = () => (
   <li className="cards__item">
     <div className="card">
       <Link to='/hvz'>
-        <div className="card__image card__image--hvz"></div>
+        <div className="card__image card__image--hvz"> </div>
         <div className="card__content">
           <div className="card__title">Mass Spatial Gaming (Humans Vs Zombies)</div>
           <p className="card__text">Fall 2016</p>
@@ -33,7 +33,7 @@ const Gallery = () => (
   <li className="cards__item">
     <div className="card">
       <Link to='/portfoliowebsite'>
-        <div className="card__image card__image--portfolio"></div>
+        <div className="card__image card__image--portfolio"> </div>
         <div className="card__content">
           <div className="card__title">Portfolio Website</div>
           <p className="card__text">Summer 2017</p>
@@ -44,7 +44,7 @@ const Gallery = () => (
   <li className="cards__item">
     <div className="card">
       <Link to='/salesforceanimation'>
-        <div className="card__image card__image--animation"></div>
+        <div className="card__image card__image--animation"> </div>
         <div className="card__content">
           <div className="card__title">Salesforce Animation Page Redesign</div>
           <p className="card__text">Summer 2017</p>
